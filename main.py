7ca118e456b8150b9eaaa52cd94bc0b4f5cda1f7
Aaron Dykes <aaronmdykes@gmail.com>
2025-02-11 00:12:32 -0500

    Finished decode cpu method

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import re
+
 class CPU:
     def __init__(self, cache_enabled=True):
         self.registers = [0]*8
         self.program_counter = 0
         self.cache_enabled = cache_enabled
-        self.instructions = []
-        self.data_memory = []
+        self.instructions = [] #list of instruction strings, ex: "ADD R1, R2, R3"
+        self.data_memory = [] #list of integers
         self.cache = [
             {'tag': None, 'data': ''},
             {'tag': None, 'data': ''},
@@ -16,7 +18,15 @@ class CPU:
         return self.instructions[self.program_counter]
     
     def decode(self, instruction_string):
-        pass
+        split_instructions = re.split(r'[ ,]+', instruction_string)
+        opcode = split_instructions[0]
+        source_register_1 = int(split_instructions[1][1:])
+        source_register_2 = int(split_instructions[2][1:])
+        destination_register = int(split_instructions[3][1:])
+        return (opcode, [source_register_1, source_register_2, destination_register])
+        
+        
+        
     
     def execute(self, opcode, operand_list):
         pass
