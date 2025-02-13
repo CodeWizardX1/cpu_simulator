0205b24f2399248e6f2557b578d5ce1918ab86af
Aaron Dykes <aaronmdykes@gmail.com>
2025-02-13 00:45:36 -0500

    Completed version 1

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import re
 
+
+
 class CPU:
-    pass
+    def __init__(self):
+        self.program_counter = 0
+        self.registers = [0]*32
+        self.memory = [] #list of instruction strings
+        self.instruction_register = ''
+        self.cache = [
+            {'tag': None, 'data': None},
+            {'tag': None, 'data': None},
+            {'tag': None, 'data': None},
+            {'tag': None, 'data': None}
+        ]
+        self.is_halted = False
+        self.current_opcode = ''
+        self.current_operands = []
+        self.temp_result = []
+        self.memory_data = []
+        
+    
+    def fetch(self):
+        self.instruction_register = self.memory[self.program_counter]
+        self.program_counter += 1
+        
+    
+    def decode(self, instruction_string):
+        pass
+        
+        
+        
+        
+    
    
     
     
